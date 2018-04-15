dd64ed5f3f41f5e01ecac3f5fce8085e631ee378
Nitesh <Nitesh>
2018-04-15 11:38:48 +0000

    First commit

---
+++ dontnetcorewebapi/dontnetcorewebapi/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using dontnetcorewebapi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dontnetcorewebapi.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        [HttpGet]
+        public IEnumerable<Employee> Employee()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            lstEmployee = objemployee.GetAllEmployees().ToList();
+
+            return lstEmployee;
+        }      
+       
+
+       
+        [HttpPost]
+        public IActionResult Post([FromBody]Employee b)
+        {
+            objemployee.AddEmployee(b);
+            return Ok();
+        }
+
+    }
+}
---
+++ dontnetcorewebapi/dontnetcorewebapi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace dontnetcorewebapi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("hosting.json", optional: true)
+        .Build();
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://localhost:5051")
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
+
+
+
+
+
